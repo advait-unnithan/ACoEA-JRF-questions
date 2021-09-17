d3d318e7b3f89e27e1ce9724743465506437c8dd
advait-unnithan <90854011+advait-unnithan@users.noreply.github.com>
2021-09-17 13:36:12 +0530

    Add files via upload

---
+++ waterfallplot.py
@@ -0,0 +1,7 @@
+%matplotlib inline
+import pylab as plt
+from blimpy import Waterfall
+
+file_path = 'Voyager1.single_coarse.fine_res.h5'
+obs = Waterfall(file_path)
+obs.plot_waterfall(f_start=8419.2740, f_stop=8419.2750)
